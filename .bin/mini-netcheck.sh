5a6399c413fe8d2bb1644cfb590f65f19fcad6bc
xaudit <105848507+Autherix@users.noreply.github.com>
2024-08-14 18:55:41 +0000

    added average for latencies + removed magic numbers + colorized better

--- .bin/mini-netcheck.sh
+++ .bin/mini-netcheck.sh
@@ -1,9 +1,16 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
 # set the timezone to asia/dubai
 export TZ="Asia/Dubai"
 
-export DEFAULT_HOST="8.8.8.8"
+# default host to ping
+DEFAULT_HOST="8.8.8.8"
+
+# number of seconds to wait between pings
+PING_INTERVAL=2
+
+# number of pings to calculate average response time
+AVERAGE_PINGS=10
 
 # if $1 is provided and it is a ip (validate with regex), set HOST to $1, else set it to DEFAULT_HOST
 if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
@@ -36,15 +43,17 @@ done
 
 shift $((OPTIND-1))
 
-round=0
+round=1
+response_times=()
 while true
 do
   printf "\e[35m%03d\e[0m %s " "$round" "$(date +%H:%M:%S)"
   # send a ping with a timeout of 2 second and save it's result in a variable called ping_result
-  ping_result_time=$(ping -c 1 -W 2 "$HOST" | grep 'time=' | cut -d '=' -f 4 | cut -d ' ' -f 1)
+  ping_result_time=$(ping -c 1 -W $PING_INTERVAL "$HOST" | grep 'time=' | cut -d '=' -f 4 | cut -d ' ' -f 1)
   
   # check if ping_result_time is not empty and command exit status is 0
   if [ -n "$ping_result_time" ] && [ $? -eq 0 ]; then
+    response_times+=("$ping_result_time")
     # print in green how much time it took
     printf "\e[32m%s ms\e[0m\n" "$ping_result_time"
   else
@@ -52,10 +61,20 @@ do
     printf "\e[31mFAIL\e[0m\n"
   fi
 
-  round=$((round+1))
-  if [ $((round % 10)) -eq 0 ]; then
+  if [ $((round % $AVERAGE_PINGS)) -eq 0 ]; then
+    # calculate average response time in last 10 seconds
+    average=$(
+      if [ ${#response_times[@]} -gt 0 ]; then
+        printf "%s\n" "${response_times[@]}" | awk '{sum+=$1; n++} END {print sum/n}'
+      else
+        echo "0"
+      fi
+    )
+    printf "\e[33mAvg: %s ms\e[0m \e[90m(average last $AVERAGE_PINGS secs)\e[0m\n" "$average"
     printf "%s\n" "------------------------------------"
+    response_times=()
   fi
-  sleep 2
+  round=$((round+1))
+  sleep $PING_INTERVAL
 done
 
